ccd300839e3758f6e869611a6f643db93cc2288d
samadvalipour <samadvalipour76@gmail.com>
2022-06-18 14:18:52 +0430

    change serilaizer to model serilaizer, add username validation to serilaizer

--- accounts/serilaizers.py
+++ accounts/serilaizers.py
@@ -1,6 +1,15 @@
+from django.forms import CharField
 from rest_framework import serializers
+from django.contrib.auth.models import User
+class UserSerilaizer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=["username","password","email"]
+        extra_kwargs = {
+            "password":{"write_only":True}
+        }
 
-class UserSerilaizer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True,write_only=True)
-    email = serializers.EmailField(required=True)
+    def validate_username(self,value):
+        if value == "admin":
+            raise serializers.ValidationError("username should not be 'admin'")
+        return value
